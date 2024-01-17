fb3b72cccb4440832893917e3743c181420b0f9a
TuInh2310Automation <tuinhle2310@gmail.com>
2024-01-18 00:43:42 +0700

    lab2

---
+++ Lab2/Lab2.1.js
@@ -0,0 +1,11 @@
+// weight in kg
+let weight = 99;
+
+// height in m
+let height = 1.52;
+
+let bmiNumber = weight / (height * 2);
+
+console.log("BMI is: ", bmiNumber);
+
+
---
+++ Lab2/Lab2.3.js
@@ -0,0 +1,26 @@
+// weight in kg
+let weight = 99;
+
+// height in m
+let height = 1.52;
+
+let bmiNumber = weight / (height * 2);
+
+console.log("BMI is: ", bmiNumber);
+
+//Underweight = < 18.5
+//Normal weight = 18.5 – 24.9
+//Overweight = 25–29.9
+//Obesity = BMI of 30 or greater
+let normalWeightMin = 18.5;
+let overweightMin = 25;
+let obesityMin = 30;
+if (bmiNumber < normalWeightMin) {
+    console.log("You are Underweight. You have to increase " + (normalWeightMin * height * 2 - weight) + "kg")
+} else if (bmiNumber < overweightMin) {
+    console.log("You have normal weight ")
+} else if (bmiNumber < obesityMin) {
+    console.log("You are Overweight.You have to reduce " + (weight - overweightMin * height * 2) + "kg")
+} else {
+    console.log("You are Obesity.You have to reduce " + (weight - overweightMin * height * 2) + "kg")
+}
