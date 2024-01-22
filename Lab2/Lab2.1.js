7bfbf0bac0750f1dff98be0d08c85d5378113fe4
Anh Thi Van Le <anhthivan.le@kofax.com>
2024-01-22 11:09:57 +0700

    Fix comment Lab2

--- Lab2/Lab2.1.js
+++ Lab2/Lab2.1.js
@@ -7,5 +7,16 @@ let height = 1.52;
 let bmiNumber = weight / (height * 2);
 
 console.log("BMI is: ", bmiNumber);
-
+let normalWeightMin = 18.5;
+let overweightMin = 25;
+let obesityMin = 30;
+if (bmiNumber < normalWeightMin) {
+    console.log("You are Underweight")
+} else if (bmiNumber < overweightMin) {
+    console.log("You have normal weight ")
+} else if (bmiNumber < obesityMin) {
+    console.log("You are Overweight")
+} else {
+    console.log("You are Obesity")
+}
 
